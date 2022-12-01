59a0e92bb0789db41f3e7674c43113c4ce515634
Atta Ur Rahman Fraooqi <73984528+iamfarooqi@users.noreply.github.com>
2022-12-01 21:32:19 +0500

    updated

--- components/projects/projects.jsx
+++ components/projects/projects.jsx
@@ -38,7 +38,7 @@ export default function Projects() {
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="sr-only">Products</h2>
+                <h2 className="sr-only">All Products</h2>
 
                 <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                     {products.map((product) => (
